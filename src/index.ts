3426e53401b496bd4277108e8e05ea4855bc5dd0
ProfetAI-NB-037\gary <gary@profetai.com>
2024-04-09 15:35:46 +0800

    refactor: replace CsvFileReader to MatchReader

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 
-const reader = new CsvFileReader("football.csv");
+const reader = new MatchReader("football.csv");
 reader.read();
 
 console.log(reader.data);
