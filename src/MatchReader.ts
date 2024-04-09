583f7ab8dc2a5fa9fe0ef22487fa7d652b63c315
ProfetAI-NB-037\gary <gary@profetai.com>
2024-04-09 18:28:52 +0800

    refactor: dependencies on  interface and create MatchReader

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,22 +1,7 @@
-import { CsvFileReader } from "./CsvFileReader";
-import { dateStringToDate } from "./util";
-import { MatchResult } from "./MatchResult";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class MatchReader extends CsvFileReader<MatchData> {
-  constructor(public filename: string) {
-    super(filename);
-  }
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, // 'H', 'A' or 'D',
-      row[6],
-    ];
-  }
+interface DataReader {
+  read(): void;
+  data: string[][];
+}
+export class MatchReader {
+  constructor(public reader: DataReader) {}
 }
