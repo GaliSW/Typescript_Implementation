5a6abed9266227035d53042bab20a3fe9a64806a
ProfetAI-NB-037\gary <gary@profetai.com>
2024-04-10 16:06:01 +0800

    refactor: composition version

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTarget/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 import { CsvFileReader } from "./CsvFileReader";
+import { HtmlReport } from "./reportTarget/HtmlReport";
 
+//Create an Object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader("football.csv");
 
+//Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
