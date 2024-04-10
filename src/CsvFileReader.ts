45317cdd686db5146fb54f4953833181fc3d44ce
ProfetAI-NB-037\gary <gary@profetai.com>
2024-04-10 14:41:30 +0800

    refactor: refactor by object composition

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,9 @@
 import fs from "fs";
-import { MatchResult } from "./MatchResult";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export abstract class CsvFileReader<TData> {
-  data: TData[] = [];
+export class CsvFileReader {
+  data: string[][] = [];
+
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): TData;
 
   read(): void {
     this.data = fs
@@ -16,7 +13,6 @@ export abstract class CsvFileReader<TData> {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      })
-      .map(this.mapRow);
+      });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader("football.csv");
 
-console.log(reader.data);
-
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
