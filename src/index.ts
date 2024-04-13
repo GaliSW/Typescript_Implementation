661af6e2a6f6e564f3ee989aab015bcdaa91a61a
GaliSW <gary850204@gmail.com>
2024-04-13 19:48:48 +0800

    update: change to console report

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ const csvFileReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+const summary = new Summary(
+    new WinsAnalysis("Man United"),
+    new ConsoleReport()
+);
 
 summary.buildAndPrintReport(matchReader.matches);
