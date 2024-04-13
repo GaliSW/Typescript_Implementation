0493fe66a7f39685b349442cc971485bb9f0e387
GaliSW <gary850204@gmail.com>
2024-04-13 12:10:42 +0800

    refactor: import MatchResult

--- src/inheritance/index.ts
+++ src/inheritance/index.ts
@@ -1,23 +1,18 @@
 import { MatchReader } from "./MatchReader";
+import { MatchResult } from "../MatchResult";
 
 const reader = new MatchReader("football.csv");
 reader.read();
 
 console.log(reader.data);
 
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
-
 let manUnitedWins = 0;
 for (let match of reader.data) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
 }
 
 console.log(`Man United won ${manUnitedWins} games`);
